c072dc14b93654e80fe2cc08acb624adf530d464
Gius <giusmile67@gmail.com>
2020-11-09 18:53:15 +0100

    modification js

--- js/app.js
+++ js/app.js
@@ -19,4 +19,42 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(typeof lang);
     console.log(typeof parseFloat(version));
     console.log(lang.length);
+
+    //comparer les types
+    console.log(false || true);
+    console.log("five" * 2); //affiche NaN Not an Number
+    console.log(false == 0); //true
+    let date = 2020;
+    let age = 21;
+    console.log(date === age); //affiche false
+    console.log(date !== age); //affiche true
+    /* 
+    liste des opérateurs
+    + additionner
+    - sous
+    ++ incrementer
+    -- décrementer
+    / diviser
+    == strictement égale
+    = affectation de valeur
+    += ajouter une valeur
+    -= enlever
+    || ou
+    && et
+    >
+    <
+    >=
+    <=
+    % modulo
+    */
+    let nombres = 10;
+    console.log(nombres % 2); //résultat
+    console.log(null == undefined);
+    let nombre = 20;
+    console.log(nombre);
+    //calculer l'année de naissance
+    nombre % 2 == 0 ? console.log("chiffre paire " + nombre) : console.log("chiffre impaire " + nombre);
+    let annee = 2020;
+    let ages = 53;
+    console.log("Vous êtes né(e) " + annee - ages);
 });
