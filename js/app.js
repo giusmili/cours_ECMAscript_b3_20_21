9f0a035ec0193f558165d70f2b03155570eba645
gius <giusmili67@gmail.com>
2020-11-22 18:11:02 +0100

    modification globale

--- js/app.js
+++ js/app.js
@@ -50,13 +50,40 @@ document.addEventListener('DOMContentLoaded', () => {
     let nombres = 10;
     console.log(nombres % 2); //résultat
     console.log(null == undefined);
-    let nombre = 20;
+    let nombre = 20; //Number(prompt("saisir un chiffre"))
     console.log(nombre);
+
     //calculer l'année de naissance
+    //condition ternaire
     nombre % 2 == 0 ? console.log(`chiffre paire ${nombre}`) : console.log(`chiffre impaire ${nombre}`);
     let annee = 2020;
     let ages = 53;
     console.log(`Vous êtes né(e) ${annee - ages}`);
 
     /* exemple dynamique */
+
+    /*
+        condition  si sinon alors
+        si code alors afficher code ou sinon message
+    */
+
+    let user = String(prompt("Saisissez votre prénom")); //méthode => fonction
+    //console.log(user)
+    let code = parseInt(prompt("Saisir le code"));
+    let result = document.querySelectorAll("p"); //dom
+    //document.getElementsByTagName("p")[0], document.getElementsByClassName(".name")[0], document.getElementById("#region"), document.getElementsByName("user")[0]
+    console.log(result);
+
+    if (isNaN(code) || !code || code === undefined) {
+        alert('Saisissez un chiffre ou remplir le champs');
+        document.location.reload();
+    } else if (!user || !isNaN(user)) {
+        alert('Saisissez des lettres ou remplir le champs');
+        document.location.reload();
+    } else {
+        console.log(`Votre code ${code} Votre ${user}`);
+        result[0].innerHTML += `Votre code est ${code}<br>Votre prénom est ${user}`;
+    }
+    console.log(result[0].childNodes[0].nodeValue); //result[0].innerText
+
 });
---
+++ js/tabs.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("loaded");
+    /* tableaux */
+    let x = 0;
+    let lang = ["html", "css", 2020]; //tab base
+    console.table(lang);
+    //les itérations dans un tableau
+    let dates = [1990, 1998, "année"];
+
+    let ensemble = [...lang, ...dates]; //spread opérateur
+
+    ensemble.push(x); //ajouter un élément
+    console.table(ensemble);
+
+    // itération init, condition et un test
+
+    let i;
+    let elements = document.querySelector("ul");
+
+    let put_print = String("");
+    for (i = 0; i < ensemble.length; i++) {
+        //méthode for
+        console.log(`${i} : ${ensemble[i]}`);
+        //elements.innerHTML+=`<li>${ensemble[i]}</li>`
+        put_print += `<li>${ensemble[i]}</li>`;
+    }
+    elements.innerHTML = put_print; //variable de référence
+
+
+    //méthode while
+    let compt = 0;
+    while (compt < ensemble.length) {
+
+        console.log(ensemble[compt]);
+        compt++;
+    }
+
+    //méthode foreach
+    ensemble.forEach((cle, valeur) => {
+        console.log(valeur + " " + cle);
+    });
+
+    //tableau multidimensionnel
+    const persons = [{
+        nom: "Berner Lee",
+        prenom: "Tim"
+    }, {
+        nom: "Each",
+        prenom: "Branden"
+    }];
+    console.table(persons[1].prenom);
+
+    for (i = 0; i < persons.length; i++) {
+        console.log(`${persons[i].nom} ${persons[i].prenom}`);
+    }
+    persons.forEach(cle => {
+        console.log(cle.nom + ' ' + cle.prenom);
+    });
+
+    for (let key in persons) {
+        console.log(key + ' ' + persons[key].nom);
+    }
+});
